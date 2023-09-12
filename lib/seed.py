f4c742f0b4ac7e72c4bd2a1be0db40446259ac65
Neema Nachok <neemanachok29@gmail.com>
2023-09-12 13:54:50 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -4,13 +4,27 @@ from faker import Faker
 import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from models import Game
-
 fake = Faker()
 
 if __name__ == '__main__':
     
+
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for i in range(50)]
+
+    session.bulk_save_objects(games)
+    session.commit()
+    session.query(Game).count()
